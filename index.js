493fd796d911a30e3e8d82f57f7a30fafec6c143
Felipe Reinaux <freinaux@number8.com>
2022-04-25 21:36:30 -0300

    version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('deploy')
    await git().add('.')
-   await git().commit("first commit!")
+   await git().commit("version")
    await git().push('origin','deploy');
 }
 
